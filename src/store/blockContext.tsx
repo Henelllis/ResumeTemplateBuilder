6c6f3722a641887f45c2f5dbf435d6bc36060b62
Henelllis <hellis1001@email.com>
2024-07-20 11:27:32 -0400

    first attempt at block context

---
+++ src/store/blockContext.tsx
@@ -0,0 +1,66 @@
+import { createContext, useState } from "react";
+import { Item } from "../types";
+
+export interface BlockContextValue {
+  blocks: {
+    headerBlockList: Array<Item>;
+    primaryBlockList: Array<Item>;
+    secondaryBlockList: Array<Item>;
+    selectionBlockList: Array<Item>;
+  };
+  setBlocks: (blocks: {
+    headerBlockList: Array<Item>;
+    primaryBlockList: Array<Item>;
+    secondaryBlockList: Array<Item>;
+    selectionBlockList: Array<Item>;
+  }) => void;
+}
+
+export const BlockContext = createContext<BlockContextValue>({
+  blocks: {
+    headerBlockList: [],
+    primaryBlockList: [],
+    secondaryBlockList: [],
+    selectionBlockList: [],
+  },
+  setBlocks: () => {},
+});
+
+function BlockContextProvider({ children }: { children: any }) {
+  const [contextValue, setContextValue] = useState<Omit<
+    BlockContextValue,
+    "setBlocks"
+  > | null>(null);
+
+  function setBlocks(
+    headerBlockList: Array<Item>,
+    primaryBlockList: Array<Item>,
+    secondaryBlockList: Array<Item>,
+    selectionBlockList: Array<Item>
+  ): void {
+    setContextValue({
+      blocks: {
+        headerBlockList,
+        primaryBlockList,
+        secondaryBlockList,
+        selectionBlockList,
+      },
+    });
+  }
+
+  const value = {
+    blocks: {
+      headerBlockList: contextValue?.blocks.headerBlockList || [],
+      primaryBlockList: contextValue?.blocks.primaryBlockList || [],
+      secondaryBlockList: contextValue?.blocks.secondaryBlockList || [],
+      selectionBlockList: contextValue?.blocks.selectionBlockList || [],
+    },
+    setBlocks,
+  };
+
+  return (
+    <BlockContext.Provider value={value}>{children}</BlockContext.Provider>
+  );
+}
+
+export default BlockContextProvider;
